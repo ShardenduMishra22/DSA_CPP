f8bf3022fc0da1207399dbe7fb8178a317726d4b
Shardendu Mishra <shardendumishrabrave@gmail.com>
2024-08-20 13:20:00 +0530

    DSA in CPP

---
+++ SegmentTree.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class SegmentTree {
+    vector<int> tree;
+    int n;
+
+    void build(const vector<int>& arr, int node, int start, int end) {
+        if (start == end) {
+            tree[node] = arr[start];
+        } else {
+            int mid = (start + end) / 2;
+            build(arr, 2 * node + 1, start, mid);
+            build(arr, 2 * node + 2, mid + 1, end);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    void update(int node, int start, int end, int idx, int val) {
+        if (start == end) {
+            tree[node] = val;
+        } else {
+            int mid = (start + end) / 2;
+            if (idx <= mid)
+                update(2 * node + 1, start, mid, idx, val);
+            else
+                update(2 * node + 2, mid + 1, end, idx, val);
+            tree[node] = tree[2 * node + 1] + tree[2 * node + 2];
+        }
+    }
+
+    int query(int node, int start, int end, int l, int r) {
+        if (r < start || l > end){
+            return 0;
+        }
+        if (l <= start && end <= r){ 
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        int left_query = query(2 * node + 1, start, mid, l, r);
+        int right_query = query(2 * node + 2, mid + 1, end, l, r);
+        return left_query + right_query;
+    }
+
+public:
+    SegmentTree(const vector<int>& arr) {
+        n = arr.size();
+        tree.resize(4 * n);
+        build(arr, 0, 0, n - 1);
+    }
+
+    void update(int idx, int val) {
+        update(0, 0, n - 1, idx, val);
+    }
+
+    int query(int l, int r) {
+        return query(0, 0, n - 1, l, r);
+    }
+};
+
+int main() {
+    vector<int> arr = {1, 3, 5, 7, 9, 11};
+    SegmentTree st(arr);
+
+    cout << st.query(1, 3) << "\n";
+    st.update(1, 10);
+    cout << st.query(1, 3) << "\n";
+    return 0;
+}
